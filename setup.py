e9f1f75df23fc5bf590df557478ec6b997af7337
lijingqing <lijingqing@cetcxl.com>
2019-11-26 11:03:54 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name='hotool',         # How you named your package folder (MyLib)
+  packages=['hotool'],   # Chose the same as "name"
+  version='0.1.0',      # Start with a small number and increase it with every change you make
+  license='GPL-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description='Tools and tricks for python work',   # Give a short description about your library
+  author='hotbless',                   # Type in your name
+  author_email='hotbless@gmail.com',      # Type in your E-Mail
+  url='https://github.com/hotbless/hotool',   # Provide either the link to your github or to your website
+  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords=['python', 'hotool'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'random',
+          'string',
+          'time',
+          'datetime'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Tools & Tricks',
+    'License :: OSI Approved :: GPL-3.0',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
